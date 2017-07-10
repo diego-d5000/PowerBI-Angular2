307f5d195ff9d5b6c72212e7a393a6cf4f3d67a5
Diego Plascencia <diego.plascencia@netlogistik.com>
2017-07-10 15:03:28 -0500

    cleaned project, deleted unnecessary samples

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,24 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SampleComponent } from './sample.component';
-import { SampleDirective } from './sample.directive';
-import { SamplePipe } from './sample.pipe';
-import { SampleService } from './sample.service';
 
 export * from './sample.component';
-export * from './sample.directive';
-export * from './sample.pipe';
-export * from './sample.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
+    SampleComponent
   ],
   exports: [
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
+    SampleComponent
   ]
 })
 export class SampleModule {
   static forRoot(): ModuleWithProviders {
     return {
-      ngModule: SampleModule,
-      providers: [SampleService]
+      ngModule: SampleModule
     };
   }
 }

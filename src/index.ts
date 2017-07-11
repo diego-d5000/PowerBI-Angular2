7625e07fbd9b902d56f9b11cab2f6b3013dd2742
Diego Plascencia <diego.plascencia@netlogistik.com>
2017-07-11 13:32:00 -0500

    modified powerbi component class name to PowerBIComponentComponent

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SampleComponent } from './sample.component';
+import { PowerBIComponentComponent } from './powerbi-component/powerbi-component.component';
 
 export * from './sample.component';
+export * from './powerbi-component/powerbi-component.component'
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
+    PowerBIComponentComponent,
     SampleComponent
   ],
   exports: [
+    PowerBIComponentComponent,
     SampleComponent
   ]
 })
--- src/powerbi-component/powerbi-component.component.spec.ts
+++ src/powerbi-component/powerbi-component.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { PowerbiComponentComponent } from './powerbi-component.component';
+import { PowerBIComponentComponent } from './powerbi-component.component';
+import 'jasmine';
 
 describe('PowerbiComponentComponent', () => {
-  let component: PowerbiComponentComponent;
-  let fixture: ComponentFixture<PowerbiComponentComponent>;
+  let component: PowerBIComponentComponent;
+  let fixture: ComponentFixture<PowerBIComponentComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PowerbiComponentComponent ]
+      declarations: [ PowerBIComponentComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PowerbiComponentComponent);
+    fixture = TestBed.createComponent(PowerBIComponentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
